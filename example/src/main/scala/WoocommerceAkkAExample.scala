8e519eb2324fbebf143875a73468bd369d54cf86
Gerbrand van Dieijen <gerbrand@vandieijen.nl>
2021-11-08 15:43:13 +0100

    Updated the example to return from a site that has a few products

--- example/src/main/scala/WoocommerceAkkAExample.scala
+++ example/src/main/scala/WoocommerceAkkAExample.scala
@@ -1,23 +1,30 @@
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import org.json4s.DefaultFormats
 import org.woocommerce.akkaclient.api.DefaultApi
-import org.woocommerce.akkaclient.core.{ApiInvoker, ApiRequest}
+import org.woocommerce.akkaclient.core.{ApiInvoker, ApiRequest, Serializers}
 import org.woocommerce.akkaclient.model
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
-
+import Seq.empty
 
 object WoocommerceAkkAExample extends App {
   implicit val system: ActorSystem = ActorSystem("Woocommerce-Akka-Example")
+  implicit val ec = system.getDispatcher
   sys.addShutdownHook(system.terminate())
+
   val invoker = ApiInvoker()
 
 
-
-  import Seq.empty
-  val storeApi = DefaultApi("https://www.software-creation.nl/wp-json/wc/store")
-  val productsGet: ApiRequest[Seq[model.Product]] = storeApi.productsGet(exclude = empty, include = empty, parent = empty, stockStatus = empty, parentExclude = empty, rating = empty, attributes = empty);
+  // Using the generated api to get a list of products
+  val storeApi = DefaultApi("https://www.liberactiva.nl/wp-json/wc/store")
+  val productsGet = storeApi.productsGet(exclude = empty, include = empty, parent = empty, stockStatus = empty, parentExclude = empty, rating = empty, attributes = empty);
   val fResult = invoker.execute(productsGet)
-  val result = Await.result(fResult, 5.seconds)
-  println(result.content.mkString)
+  val result = Await.result(fResult, 15.seconds)
+  println(result.content)
+  System.exit(0)
 }
